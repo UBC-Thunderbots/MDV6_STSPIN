14040182ed825fbba1b6278e61600272ebe87201
x4132 <31595285+x4132@users.noreply.github.com>
2025-04-14 17:25:11 -0700

    update vscode configs

--- Src/firmware.c
+++ Src/firmware.c
@@ -1,5 +1,6 @@
 #include "firmware.h"
 #include "../common/types.h"
+#include "stm32f0xx_hal.h"
 #include "stm32f0xx_hal_spi.h"
 #include <stdint.h>
 
@@ -8,8 +9,12 @@ struct RawMessage {
     uint8_t counter;
 };
 
-enum OPCODES poll(SPI_HandleTypeDef *hspi) {
-    struct RawMessage msg = {0};
+enum OPCODES mc_SPI_poll(SPI_HandleTypeDef *hspi) {
 }
+
+enum OPCODES __mc_SPI_readMessage(SPI_HandleTypeDef *hspi) {
+
+}
+
 uint8_t SPITransmit(uint8_t tx) {
 }
